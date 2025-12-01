8a6a257859a782680e5dc21d476670bd0b69df1f
scvds123645 <270fe019@gotrak.org>
2025-12-01 17:58:14 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// 1. 更改这里：引入 HashRouter 而不是 BrowserRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import Products from "./pages/Products";
@@ -13,9 +14,8 @@ import TextDeduplicator from "./pages/TextDeduplicator";
 import EmailDomainFormatter from "./pages/EmailDomainFormatter";
 import DiscordFormatter from "./pages/DiscordFormatter";
 import NumberExtractor from "./pages/NumberExtractor";
-import NumberGenerator from "./pages/NumberGenerator"; // Import added here
+import NumberGenerator from "./pages/NumberGenerator"; 
 import SoftwareDownload from "./pages/SoftwareDownload";
-import Zd180 from "./pages/zd180";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,25 +25,26 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* 2. 更改这里：使用 HashRouter，并移除 basename */}
+      {/* 注意：HashRouter 不需要 basename="/test-584136"，它会自动处理 # 后面的内容 */}
+      <HashRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Products />} />
           <Route path="/tools" element={<Tools />} />
-          <Route path="/180" element={<Zd180 />} />
           <Route path="/jh" element={<CookieFilter />} />
           <Route path="/cookie" element={<CookieConverter />} />
           <Route path="/qc" element={<TextDeduplicator />} />
           <Route path="/yopmail" element={<EmailDomainFormatter />} />
           <Route path="/discord" element={<DiscordFormatter />} />
           <Route path="/14" element={<NumberExtractor />} />
-          <Route path="/14d" element={<NumberGenerator />} /> {/* Route added here */}
+          <Route path="/14d" element={<NumberGenerator />} />
           <Route path="/rj" element={<SoftwareDownload />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
